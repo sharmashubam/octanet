8444dc5119eeef5795c41e048d2c283347f2b1a8
Your Name <you@example.com>
2023-09-11 22:11:11 +0530

    octa

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNavbar() {
+	document.getElementById("sideNavigationBar")
+		.style.width = "50%";
+}
+function closeNavbar() {
+	document.getElementById("sideNavigationBar")
+		.style.width = "0%";
+}
+function myFunction(x) {
+	x.classList.toggle("change");
+  }
